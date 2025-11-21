7908179617416d80d58319085603009354c6bed8
stryngs <support@configitnow.com>
2025-11-21 18:13:16 -0500

    sync point

--- SRC/DEB_picopilot-unmanned/opt/piCopilot-unmanned/scripts/gStreamerWrapper.sh
+++ SRC/DEB_picopilot-unmanned/opt/piCopilot-unmanned/scripts/gStreamerWrapper.sh
@@ -1,18 +1,10 @@
 #!/bin/bash
 
 ## ./gStreamWrapper.sh 640 480 192.168.10.10
+## viewer -> gst-launch-1.0 -v udpsrc port=5600 caps="application/x-rtp,media=video,encoding-name=H264,payload=96" ! rtph264depay ! avdec_h264 ! videoconvert ! autovideosink sync=false
 
 ## Detect current stream states
-motionStatus=$(netstat -tulpn | grep 8081 | grep motion | cut -d\/ -f2)
 vmStatus=$(lsmod | grep bcm2835_v4l[2] | head -n 1 | awk '{print $1}')
-gStatus=$(ps aux | grep -o raspivi[d] | head -n 1)
-
-## Stop motion
-if [[ "$motionStatus" == 'motion' ]]; then
-  systemctl stop motioneye
-  sleep 1
-  systemctl stop motion
-fi
 
 ## Kill any prior resolutions
 killall -9 raspivid
@@ -21,6 +13,6 @@ killall -9 raspivid
 [[ "$vmStatus" != "bcm2835_v4l2" ]] && modprobe bcm2835-v4l2
 
 ## GStreamer
-raspivid -n -fl -w $1 -h $2 -b 10000000 -fps 30 -t 0 -rot 0 --exposure auto -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=$3 port=5600
+# raspivid -n -fl -w $1 -h $2 -b 10000000 -fps 30 -t 0 -rot 0 --exposure auto -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=$3 port=5600
+rpicam-vid -n --inline -t 0 --width $1 --height $2 --framerate 30 --bitrate 4000000 --rotation 0 --exposure normal -o - | gst-launch-1.0 -v fdsrc ! h264parse ! rtph264pay config-interval=10 pt=96 ! udpsink host=$3 port=5600
 echo "GStreamer" > /tmp/videoStream
-# fi
--- SRC/DEB_picopilot/usr/bin/copilotDhcp.sh
+++ SRC/DEB_picopilot/usr/bin/copilotDhcp.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
 
-## Set DHCP per the expected schema
-cat << "EOF" > /etc/dnsmasq.conf
-dhcp-range=192.168.10.2,192.168.10.253,255.255.255.0,12h
-#dhcp-option=option:router,192.168.10.254
-#dhcp-option=option:dns-server,192.168.10.254
-dhcp-authoritative
-EOF
-
-## Stop and start the DNS
-systemctl stop dnsmasq
+## Start the DNS
 systemctl start dnsmasq
